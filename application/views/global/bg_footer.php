b3309bcb025108f307bb20fc8f95866572af86c6
Afrianz <afrian.anjaya.saputra@gmail.com>
2017-12-20 08:20:35 +0700

    footer

--- application/views/global/bg_footer.php
+++ application/views/global/bg_footer.php
@@ -1,7 +1,7 @@
 
 
 	<p class="footer">
-	Sistem Informasi Akademik Online 2012 - Gede Lumbung (http://gedelumbung.com)<br />
+	Sistem Informasi KRS Online Universitas Muhammadiyah 2017<br />
 	Halaman ini dimuat selama <strong>{elapsed_time}</strong> detik
 	</p>
 </div>
